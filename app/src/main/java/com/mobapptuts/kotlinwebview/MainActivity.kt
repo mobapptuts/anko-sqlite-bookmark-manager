0772e8354907330ddf811948d533b546e8d36c98
Nigel Henshaw <nigel@mobapptuts.com>
2017-10-06 14:00:29 +0900

    Setting up the base source for bookmark manager

--- app/src/main/java/com/mobapptuts/kotlinwebview/MainActivity.kt
+++ app/src/main/java/com/mobapptuts/kotlinwebview/MainActivity.kt
@@ -12,21 +12,7 @@ import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.browser_toolbar.*
 
-class MainActivity : AppCompatActivity(), HistoryDialogFragment.WebHistory {
-
-    override fun getWebView() = webview
-
-/*
-    override fun webpageSelected(webTitle: String) {
-        val webHistory = webview.copyBackForwardList()
-        for (i in 0 until webHistory.size) {
-            if (webHistory.getItemAtIndex(i).title.equals(webTitle)) {
-                webview.goBackOrForward(i - webHistory.currentIndex)
-                break
-            }
-        }
-    }
-*/
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,28 +24,6 @@ class MainActivity : AppCompatActivity(), HistoryDialogFragment.WebHistory {
                 true
             } else false
         }
-        backButton.setOnClickListener {
-            if(webview.canGoBack()) webview.goBack()
-        }
-        forwardButton.setOnClickListener {
-            if(webview.canGoForward()) webview.goForward()
-        }
-        backButton.setOnLongClickListener {
-            getHistoryDialog(true)
-            true
-        }
-        forwardButton.setOnLongClickListener {
-            getHistoryDialog(false)
-            true
-        }
-    }
-
-    fun getHistoryDialog(backAdapter: Boolean) {
-        val historyDialogFragment = HistoryDialogFragment()
-        val bundle = Bundle()
-        bundle.putBoolean(historyDialogFragment.selectBackAdapter, backAdapter)
-        historyDialogFragment.arguments = bundle
-        historyDialogFragment.show(supportFragmentManager, "HistoryDialog")
     }
 
     @Throws(UnsupportedOperationException::class)
